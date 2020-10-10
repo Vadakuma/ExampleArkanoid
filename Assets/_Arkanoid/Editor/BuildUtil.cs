ff195d1e0515a958fffb4e409e5796a201f19af4
Vadim Bezrukov <vadim@tvori.co>
2020-10-10 15:31:38 +0300

    - added build util
     - update packages

---
+++ Assets/_Arkanoid/Editor/BuildUtil.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BuildUtil
+{
+    static void PerformBuild()
+    {
+        var report = BuildPipeline.BuildPlayer(
+            new[] { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/Game.unity" },
+            "Build/Win/Test.exe",
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None);
+
+        Debug.Log(report);
+    }
+}
